18f2d803f092575658c527cc01c1f2d5bf713dfd
Nicholas Smith <nick@nicholassmith.me.uk>
2015-12-04 12:25:21 +0000

    Added day 2 part 2

--- day3.py
+++ day3.py
@@ -14,6 +14,12 @@ x = 0
 y = 0
 
 houseMap = {}
+santaRoboMap = {}
+
+santaX = 0
+santaY = 0
+roboX = 0
+roboY = 0
 
 for char in inputString:
 	if char == '^':
@@ -27,4 +33,32 @@ for char in inputString:
 
 	houseMap[y,x] = 1
 
+x = 0
+while(x < len(inputString)):
+	if(inputString[x] == '^'):
+		santaY = santaY + 1
+	elif(inputString[x] == 'v'):
+		santaY = santaY - 1
+	elif(inputString[x] == '>'):
+		santaX = santaX + 1
+	elif(inputString[x] == '<'):
+		santaX = santaX - 1
+
+	santaRoboMap[santaY,santaX] = 1
+
+	if(inputString[x+1] == '^'):
+		roboY = roboY + 1
+	elif(inputString[x+1] == 'v'):
+		roboY = roboY - 1
+	elif(inputString[x+1] == '>'):
+		roboX = roboX + 1
+	elif(inputString[x+1] == '<'):
+		roboX = roboX - 1
+
+	santaRoboMap[roboY,roboX] = 1
+
+	x = x + 2
+
+
 print "number of houses with at least 1 present: ", len(houseMap)
+print "number of houses with at least 1 present with robo Santa: ", len(santaRoboMap)
