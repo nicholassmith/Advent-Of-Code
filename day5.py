7ef75602547fff7eb6208130034a5dbd198d3fee
Nicholas Smith <nick@nicholassmith.me.uk>
2015-12-06 20:52:52 +0000

    Not spitting out the right result for p2

--- day5.py
+++ day5.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys, argparse, re
+from collections import Counter
 
 parser = argparse.ArgumentParser(description='Nice and naughty strings.')
 parser.add_argument('-i', '--input', help='Input of strings.', required=True)
@@ -8,22 +9,37 @@ args = parser.parse_args()
 
 inputFile = open(args.input, 'rb')
 
-niceStrings = 0
+niceStrings = []
+
+partialStrings = []
 
 for line in inputFile:
 	fileLine = line.strip()
-	naughtyGroups = re.compile('(ab|cd|pq|xy)')
-	if(naughtyGroups.search(fileLine)) is None:
-		vowels = re.compile('[aeiou]')
-		matches = re.findall(vowels, fileLine)
-		if(len(matches) >= 3):
-			n = 2
-			print fileLine.strip()
-			for i in range(0, len(fileLine.strip())):
-				subset = fileLine[i:i+n]
-				print subset
-				if(subset.endswith(subset[0]) and len(subset) == 2):
-					niceStrings = niceStrings + 1
-					break
+
+	n = 3
+	for i in range(0, len(fileLine.strip())):
+		subset = fileLine[i:i+n]
+		if(subset.endswith(subset[0]) and len(subset) == 3):
+			print fileLine, subset
+			partialStrings.append(fileLine)
+			break
+
+for string in partialStrings:
+	n = 2
+	doubleChars = []
+	for i in range(0, len(string), n):
+		doubleChars.append(string[i:i+n])
+
+	dupes = [k for k,v in Counter(doubleChars).items() if v>1]
+	if(dupes):
+		print "our dupes", dupes
+		print "string is:", doubleChars
+		index = doubleChars.index(dupes[0])
+		if(index != len(doubleChars)):
+			if(doubleChars[index+1].startswith(doubleChars[index][1])):
+				print "overlap for", doubleChars
+			else:
+				niceStrings.append(string)
+				
 			
-print niceStrings
+print len(niceStrings)
