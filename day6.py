4cf2252dbc24057dccce9bfd2527dd52dae19267
Nicholas Smith <nick@nicholassmith.me.uk>
2015-12-10 16:03:34 +0000

    Day 6 pt 2

--- day6.py
+++ day6.py
@@ -8,7 +8,7 @@ args = parser.parse_args()
 
 inputFile = open(args.input, 'rb')
 
-lightMap = [[False for i in range(1000)] for j in range(1000)]
+lightMap = [[0 for i in range(1000)] for j in range(1000)]
 
 for line in inputFile:
 	tokens = line.strip().split(' ')
@@ -22,7 +22,7 @@ for line in inputFile:
 			xEnd = int(endVals[1])
 			for j in range(xStart, xEnd+1):
 				for i in range(yStart, yEnd+1):
-					lightMap[j][i] = True
+					lightMap[j][i] = lightMap[j][i]+1
 		elif(tokens[1] == 'off'):
 			startVals = tokens[2].split(',')
 			yStart = int(startVals[0])
@@ -32,7 +32,9 @@ for line in inputFile:
 			xEnd = int(endVals[1])
 			for j in range(xStart, xEnd+1):
 				for i in range(yStart, yEnd+1):
-					lightMap[j][i] = False
+					lightMap[j][i] = lightMap[j][i]-1
+					if lightMap[j][i] < 0:
+						lightMap[j][i] = 0
 	elif(tokens[0] == 'toggle'):
 		startVals = tokens[1].split(',')
 		yStart = int(startVals[0])
@@ -42,8 +44,7 @@ for line in inputFile:
 		xEnd = int(endVals[1])
 		for j in range(xStart, xEnd+1):
 				for i in range(yStart, yEnd+1):
-					currentVal = lightMap[j][i]
-					lightMap[j][i] = not currentVal
+					lightMap[j][i] = lightMap[j][i] + 2
 
 flatMap = list(itertools.chain(*lightMap))
 
