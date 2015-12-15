bf82476d254a103998fdd07fb99b96a6aaa21ff3
Nicholas Smith <nick@nicholassmith.me.uk>
2015-12-15 15:34:23 +0000

    Day 8 pt2

--- day8.py
+++ day8.py
@@ -9,13 +9,18 @@ args = parser.parse_args()
 inputFile = open(args.input, 'rb')
 
 codeChars = 0
-stringChars = 0 
+stringChars = 0
+encodedChars = 0 
 
 for line in inputFile:
 	codeChars = codeChars + len(line.strip())
+	print line.strip().encode('string_escape')
 	stringChars = stringChars + len(eval(line.strip()))
+	encodedChars = encodedChars + 2 + line.count('\\') + line.count('"') + len(line.strip())
 
 
 print 'codeChars count', codeChars
 print 'stringChars count', stringChars
-print 'final: ', codeChars - stringChars
+print 'encodedChars count', encodedChars
+print 'final pt1: ', codeChars - stringChars
+print 'final pt2:', encodedChars - codeChars
